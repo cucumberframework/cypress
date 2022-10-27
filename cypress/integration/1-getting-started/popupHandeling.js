4b9455b2468726d21f8fa45a546134def5098f83
Rahul Barapatre <b03810742@gmail.com>
2022-10-27 21:03:34 +0530

    Adding test cases to handle popup

---
+++ cypress/integration/1-getting-started/popupHandeling.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress"/>
+
+//const { get } = require("cypress/types/lodash");
+
+describe('Automation practice', function () {
+
+    it('popupHandeling', function () {
+        // code to check and uncheck the checkboxes---------------------------------->>>>>>>>>>>>>>>>>>
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+        cy.get("#alertbtn").click();
+        cy.get("#confirmbtn").click();
+        cy.on("window:alert", (str) => {
+            //Simple Alert --compare the string which we get from the alert box with the expected string 
+            expect(str).to.equal("Hello , share this practice page and share your knowledge");
+        })
+            //Confirmation alert -- 
+        cy.on("window.confirm",(str)=>{
+            expect(str).to.equal("Hello , Are you sure you want to confirm?")
+        })
+
+    })
+})
