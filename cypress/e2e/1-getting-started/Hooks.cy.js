9f9c9cc1fe525b19198d75534eb6e877c514d6eb
cucumberframework <66793368+cucumberframework@users.noreply.github.com>
2022-11-08 21:00:41 +0530

    Added test case extracting data from external files-fixtures, how to get the value of the attribute, and test cases coding
    Added test case extracting data from external files-fixtures
     how to get the value of the attribute
    and test cases coding

--- cypress/e2e/1-getting-started/Hooks.cy.js
+++ cypress/e2e/1-getting-started/Hooks.cy.js
@@ -6,25 +6,35 @@ import 'cypress-iframe'
 //Tutorial-43
 describe('Tutorial-43-Hooks Explaination', function () {
 
-    before(function () {
-        cy.log("This is before function");
-        cy.visit('https://ui.cogmento.com');
-        cy.get("input[name='email']").type("rahulscreencast9892@gmail.com");
-        cy.get("input[name='password']").type("Ra987456321@"); 
-        cy.contains("Login").click(); 
-        //cy.contains("Sign in").click(); 
-    })
+   
 
     beforeEach(function () {
-        cy.log("This is beforeEach hook ")
+        /* code for fixtures- -taking data from external files  */
+        cy.fixture("loginns").then(function (loginData) {
+            this.loginData = loginData;
+        })
         
+
     })
 
-    it('Iframe hendeling', function () {
+    it('Creating new contact', function () {
+
+        cy.log("This is before function");
+        cy.visit(this.loginData.url);
+        cy.get("input[name='email']").type(this.loginData.email);
+        cy.get("input[name='password']").type(this.loginData.password);
+        cy.contains("Login").click();
         cy.get("i[class='users icon']").click();
-        cy.get("a[href='/contacts/new']").click(); 
+        cy.get("a[href='/contacts/new']").click();
+        cy.get("input[name='first_name']").type(this.loginData.FirstName)
 
-
+        /* below line will get the attribute value and will compare if that value is equal or not-- i.e get the value of the attribute and compare it with the expected one  */
+        
+        cy.get("input[name='first_name']").should("have.attr", "autocomplete", "new-password");
+        cy.get("input[name='last_name']").type(this.loginData.LastName); 
+        cy.get("input[name='middle_name']").type(this.loginData.MiddleName)
+        cy.get("div[name='company']").type(this.loginData.Company);
+        cy.contains("Save").click();
 
     })
 
