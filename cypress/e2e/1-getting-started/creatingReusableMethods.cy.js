6edd550b4f4dd9530a387431b9fe7c4aa1284840
cucumberframework <66793368+cucumberframework@users.noreply.github.com>
2022-11-22 21:10:06 +0530

    Added changes which can be ignored

--- cypress/e2e/1-getting-started/creatingReusableMethods.cy.js
+++ cypress/e2e/1-getting-started/creatingReusableMethods.cy.js
@@ -8,8 +8,8 @@ import HomePage from './PageObject/HomePage.cy'
 //Tutorial-47,Tut-48 
 describe('Tutorial-43 to Tutorial -47--Hooks Explaination', function () {
 
-    const homePage= new HomePage(); 
-    const contactDetail=new ContactDetails();
+    const homePage = new HomePage();
+    const contactDetail = new ContactDetails();
 
     beforeEach(function () {
         /* code for fixtures- -taking data from external files  */
@@ -21,21 +21,23 @@ describe('Tutorial-43 to Tutorial -47--Hooks Explaination', function () {
     })
 
     it('loggin in into application with customised code', function () {
-
+        s
         cy.log("This is before function");
 
         /* Code to parameterise out test cases 
         this.loginData.email.forEach(emailAddress=>{ */
         cy.LoginIntoApp(this.loginData.url, this.loginData.email, this.loginData.password);
-        
-        
         homePage.getContactsIcon().click();
         homePage.getCreateContactButton().click();
-        contactDetail.getFirstName().type(this.loginData.FirstName); 
-        contactDetail.getLastName().type(this.loginData.LastName); 
+        contactDetail.getFirstName().type(this.loginData.FirstName);
+        contactDetail.getLastName().type(this.loginData.LastName);
         contactDetail.getMiddleName().type(this.loginData.MiddleName);
         contactDetail.getSaveButton().click();
-        homePage.getSettingsButton().click(); 
+        cy.pause();
+        cy.get("i[class='settings icon']").invoke('show')
+        cy.contains("Log Out").click();
+        Cypress.config('defaultCommandTimeout', 10000);                              //This command will have hte default 
+        //timeout of 10 sec for this test case only if we want to apply for all the test cases then we need to mantion in hte cypress.config.j
         homePage.getLogOutButton().click();
         homePage.getSearchField().type('this.loginData.FirstName{enter}')
 
